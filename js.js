7311c76acb97e71c6c83c4ba228224975f0eebe8
Mikle117 <mikle1117111@gmail.com>
2024-11-01 16:31:58 +0300

    Add files via upload

--- js.js
+++ js.js
@@ -1,7 +1,7 @@
 /* Это объявление переменной, мы наши кнопку по тегу */
-const button = document.querySelector('button');
+const button1 = document.querySelector('button');
 
 /* Тут на кнопку навешиваем обрабочик, который ждёт клика и тогда запустит логику */
-button.addEventListener('click', function() {
+button1.addEventListener('click', function() {
 	alert('Я сам вас найду...')
 })
