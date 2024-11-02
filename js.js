01ca90e4545e1458bab6dc7e63024035ee303996
Mikle117 <mikle1117111@gmail.com>
2024-11-02 08:40:30 +0300

    Update js.js

--- js.js
+++ js.js
@@ -3,5 +3,5 @@ const button1 = document.querySelector('button');
 
 /* Тут на кнопку навешиваем обрабочик, который ждёт клика и тогда запустит логику */
 button1.addEventListener('click', function() {
-	alert('Я сам вас найду...')
+	alert('Я сам тебя найду...')
 })
